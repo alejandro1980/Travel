c21ef5e6344c87cb76391596009ea6ea645f9db3
l l <alejandromigueldelvecchio@gmail.com>
2024-08-28 10:46:05 -0300

    backend

--- AMV_Travel/Program.cs
+++ AMV_Travel/Program.cs
@@ -4,31 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuración del servicio de base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Registro de servicios de aplicación
 builder.Services.AddScoped<GestorReservasService>();
 
-// Configuración de controladores y Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuración de CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins", policyBuilder =>
-        policyBuilder.AllowAnyOrigin()  // Permitir cualquier origen
-                     .AllowAnyMethod()  // Permitir cualquier método (GET, POST, etc.)
-                     .AllowAnyHeader()); // Permitir cualquier encabezado
+        policyBuilder.AllowAnyOrigin()  
+                     .AllowAnyMethod()  
+                     .AllowAnyHeader()); 
 });
 
 var app = builder.Build();
 app.UseCors("AllowAllOrigins");
 
-// Configuración para entorno de desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -37,12 +32,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Aplicar la política CORS antes de Authorization
-//app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
-// Configuración del enrutamiento
 app.MapControllers();
 
 app.Run();
